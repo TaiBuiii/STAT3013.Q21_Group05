f263297f1cf5e7e2b32fcf59a86577cc45952472
Tai Bui <taibuiii2410@gmail.com>
2026-04-27 20:13:55 +0700

    fix datapreparation

--- notebooks/data_preparation.py
+++ notebooks/data_preparation.py
@@ -2,25 +2,26 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def data_preparation(file_path):
-    # Load data
     df = pd.read_csv(file_path)
-    
-    # Filter irrevalent variables
     df = df.drop(columns=["CustomerID", "AdvertisingPlatform", "AdvertisingTool"])
 
     FEATURES = [
-        "CampaignType", "AdSpend", "ClickThroughRate", "WebsiteVisits", 
-        "PagesPerVisit", "TimeOnSite", "EmailOpens", "EmailClicks", 
-        "PreviousPurchases", "LoyaltyPoints"
+        'CampaignType', 'AdSpend', 'ClickThroughRate', 'WebsiteVisits', 
+        'PagesPerVisit', 'TimeOnSite', 'EmailOpens', 'EmailClicks', 
+        'PreviousPurchases', 'LoyaltyPoints'
     ]
-    TARGET = "Conversion"
+    TARGET = 'Conversion'
     
     X = df[FEATURES].copy()
     y = df[TARGET].copy()
 
-    # Split train and test dataset
-    X_train, X_test, y_train, y_test = train_test_split(
+    # Split dataset into training and testing datasets
+    X_temp, X_test, y_temp, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
+    # Split training dataset into training and validation datasets
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_temp, y_temp, test_size=0.2, random_state=42, stratify=y_temp
+    )
     
-    return X_train, X_test, y_train, y_test
+    return X_train, X_val, X_test, y_train, y_val, y_test
